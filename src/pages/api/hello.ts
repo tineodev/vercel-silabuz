4ab85a86254683e7e6216627fa6d525dce8eae98
tineodev <tineo.dev@gmail.com>
2023-01-26 15:45:28 -0500

    demo 1

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse, NextRequest } from "next/server";
 
-type Data = {
-  name: string
-}
+export const config = {
+  runtime: "edge",
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextRequest) => {
+  return NextResponse.json({
+    message: `Hello World ${req.url}`,
+  });
+};
